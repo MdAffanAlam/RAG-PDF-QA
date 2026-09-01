7126a7ad1119a1fd4e47294e39ed96a6e51ea744
affan <affanalam2019>
2026-09-01 13:20:27 +0530

    ChromaDb Work Done

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import path from "node:path";
 import { fileURLToPath } from "url";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-import { MemoryVectorStore } from "@langchain/classic/vectorstores/memory";
+// import { MemoryVectorStore } from "@langchain/classic/vectorstores/memory";
+import { CloudClient } from "chromadb";
+import "dotenv/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -38,6 +40,9 @@ class PdfQA {
 
   async loadPdf() {
     console.log("Loading Documents...");
+    console.log("---------------------------------------------------");
+    console.log("---------------------------------------------------");
+
     const loader = new PDFLoader(path.join(__dirname, this.pdfPath));
 
     this.docs = await loader.load();
@@ -46,6 +51,9 @@ class PdfQA {
   }
 
   async split() {
+    console.log("Splitting Documents...");
+    console.log("---------------------------------------------------");
+    console.log("---------------------------------------------------");
     const splitter = new RecursiveCharacterTextSplitter({
       separators: " ",
       chunkSize: this.chunkSize,
@@ -57,15 +65,92 @@ class PdfQA {
 
   async createVectorStore() {
     console.log("Creating document embeddings...");
-    this.db = await MemoryVectorStore.fromDocuments(
-      this.texts,
-      this.embeddingModel
-    );
-    const retriever = this.db.asRetriever(3);
+    console.log("---------------------------------------------------");
+    console.log("---------------------------------------------------");
 
-    const retrievedDocuments = await retriever.invoke("What is Font?");
+    //this is for local database in langchain
+    // this.db = await MemoryVectorStore.fromDocuments(
+    //   this.texts,
+    //   this.embeddingModel
+    // );
+    // console.log("db :-",this.db)
+    // const retriever = this.db.asRetriever(2);
 
-    console.log("result  :-  ",retrievedDocuments);
+    // const retrievedDocuments = await retriever.invoke("What is Font?");
+
+    // console.log("result  :-  ",retrievedDocuments);
+
+    this.client = new CloudClient({
+      apiKey: process.env.CHROMA_API_KEY,
+      tenant: process.env.CHROMA_TENANT,
+      database: process.env.CHROMA_DATABASE,
+    });
+
+    await this.client.heartbeat();
+
+    console.log("Chroma Cloud connected!");
+    console.log("---------------------------------------------------");
+    console.log("---------------------------------------------------");
+
+    await this.createCollection();
+    return this;
+  }
+
+  async createCollection() {
+    console.log("Create collection...");
+    console.log("---------------------------------------------------");
+    console.log("---------------------------------------------------");
+    this.collection = await this.client.getOrCreateCollection({
+      name: "pdf_documents",
+      embeddingFunction: null,
+    });
+    console.log("Collection Name :- ", this.collection.name);
+    console.log("---------------------------------------------------");
+    console.log("---------------------------------------------------");
+    await this.addDocuments();
+    return this;
+  }
+
+  async addDocuments() {
+    //Extract content from documents
+    this.content = this.texts.map((doc) => doc.pageContent);
+
+    //Unique ids
+    this.ids = this.texts.map((_, index) => {
+      return `ID${index}`;
+    });
+
+    //create embeddings for each documents
+    this.embedds = await this.embeddingModel.embedDocuments(this.content);
+
+    await this.collection.add({
+      ids: this.ids,
+      documents: this.content,
+      embeddings: this.embedds,
+    });
+
+    console.log("PDF successfully stored in Chroma Cloud!");
+
+    console.log("---------------------------------------------------");
+    console.log("---------------------------------------------------");
+
+    await this.queryDocuments();
+    return this;
+  }
+
+  async queryDocuments() {
+    //get documents from CHROMA DATABASE
+    const queryTxt = "What is Font?";
+    const queryEmbedding = await this.embeddingModel.embedQuery(queryTxt);
+    // console.log("queryEmbedding :-",queryEmbedding)
+
+    this.results = await this.collection.query({
+      queryEmbeddings: [queryEmbedding],
+      nResults: 4,
+    });
+    console.log("Results :- ", this.results);
+    console.log("---------------------------------------------------");
+    console.log("---------------------------------------------------");
   }
 }
 
